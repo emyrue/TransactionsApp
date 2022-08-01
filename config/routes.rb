fad60e0aff3b1ae922a80db6f5eefdb5e6375a40
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-01 14:05:19 -0600

    Generated devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   resources :group_expenses
   resources :groups
   resources :expenses
