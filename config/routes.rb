f3484b491ed7e9f708af5fa7d1b4a6dae4af6508
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-01 15:31:20 -0600

    Completed splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root "home#splash_screen"
   # Defines the root path route ("/")
   # root "articles#index"
 end
